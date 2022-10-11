15d4674fd2f2cd3204155edd8b9aecf1cba0ac73
Orgil <orgil@controls-Mac-mini.local>
2022-10-11 21:52:52 +0800

    push

---
+++ netflix-clone/pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Netflix - mimic</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Header/>
+        {/* Banner */}
+        
+        <section>
+
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+
+        </section>
+      </main>
+
+      {/* Modal */}
+
+    </div>
+  )
+}
+
+export default Home
